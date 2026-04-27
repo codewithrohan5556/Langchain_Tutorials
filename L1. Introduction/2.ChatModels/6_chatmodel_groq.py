40e193191c7eaab4fce3fb18eeefab8df48a3c7c
Rohan Patil <166194152+codewithrohan5556@users.noreply.github.com>
2026-04-27 12:21:48 +0530

    Delete L1. Introduction directory

--- L1. Introduction/2.ChatModels/6_chatmodel_groq.py
+++
@@ -1,9 +0,0 @@
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = ChatGroq(model='llama-3.3-70b-versatile')
-
-result = model.invoke('What is the capital of Maharashtra?')
-print(result)
